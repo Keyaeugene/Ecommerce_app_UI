da3a37dcc9610992d14e70c84f48a28f835a4981
keyaeugene <keyaeugene@gmail.com>
2024-01-26 19:35:12 +0300

    menu button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sneakertute/components/bottom_nav_bar.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  //this method will update our selected index
+  //when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //pages to display
+  final List<Widget> _pages = [
+    //shop page
+    const ShopPage(),
+
+    //cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+      body: _pages[_selectedIndex],
+    );
   }
 }
